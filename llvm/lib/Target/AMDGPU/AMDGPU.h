477a0f1dd42b20e5303a2b7ac68d2bf57ec7a47a
Matt Arsenault <Matthew.Arsenault@amd.com>
2022-12-23 16:55:40 -0500

    AMDGPU: Move enqueued block handling into clang
    The previous implementation wasn't maintaining a faithful IR
    representation of how this really works. The value returned by
    createEnqueuedBlockKernel wasn't actually used as a function, and
    hacked up later to be a pointer to the runtime handle global
    variable. In reality, the enqueued block is a struct where the first
    field is a pointer to the kernel descriptor, not the kernel itself. We
    were also relying on passing around a reference to a global using a
    string attribute containing its name. It's better to base this on a
    proper IR symbol reference during final emission.
    
    This now avoids using a function attribute on kernels and avoids using
    the additional "runtime-handle" attribute to populate the final
    metadata. Instead, associate the runtime handle reference to the
    kernel with the !associated global metadata. We can then get a final,
    correctly mangled name at the end.
    
    I couldn't figure out how to get rename-with-external-symbol behavior
    using a combination of comdats and aliases, so leaves an IR pass to
    externalize the runtime handles for codegen. If anything breaks, it's
    most likely this, so leave avoiding this for a later step. Use a
    special section name to enable this behavior. This also means it's
    possible to declare enqueuable kernels in source without going through
    the dedicated block syntax or other dedicated compiler support.
    
    We could move towards initializing the runtime handle in the
    compiler/linker. I have a working patch where the linker sets up the
    first field of the handle, avoiding the need to export the block
    kernel symbol for the runtime. We would need new relocations to get
    the private and group sizes, but that would avoid the runtime's
    special case handling that requires the device_enqueue_symbol metadata
    field.
    
    https://reviews.llvm.org/D141700

--- llvm/lib/Target/AMDGPU/AMDGPU.h
+++ llvm/lib/Target/AMDGPU/AMDGPU.h
@@ -444,9 +444,9 @@ void initializeAMDGPUExternalAAWrapperPass(PassRegistry&);
 
 void initializeAMDGPUArgumentUsageInfoPass(PassRegistry &);
 
-ModulePass *createAMDGPUOpenCLEnqueuedBlockLoweringLegacyPass();
-void initializeAMDGPUOpenCLEnqueuedBlockLoweringLegacyPass(PassRegistry &);
-extern char &AMDGPUOpenCLEnqueuedBlockLoweringLegacyID;
+ModulePass *createAMDGPUExportKernelRuntimeHandlesLegacyPass();
+void initializeAMDGPUExportKernelRuntimeHandlesLegacyPass(PassRegistry &);
+extern char &AMDGPUExportKernelRuntimeHandlesLegacyID;
 
 void initializeGCNNSAReassignLegacyPass(PassRegistry &);
 extern char &GCNNSAReassignID;
